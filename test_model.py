63175f5a776df6725e69c3e988743666550dbfba
PyDevC <anuskcse@gmail.com>
2025-03-24 00:10:31 +0530

    fixed predictor

--- test_model.py
+++ test_model.py
@@ -13,9 +13,8 @@ def digit_to_classname(digit):
     gesture_folders = ['01_palm', '02_l', '03_fist', '04_fist_moved', '05_thumb',
                    '06_index', '07_ok', '08_palm_moved', '09_c', '10_down']
 
-    print(digit)
     for idx, itm in enumerate(gesture_folders):
-        if idx == digit.argmax():
+        if idx == digit:
             return itm
 
 
