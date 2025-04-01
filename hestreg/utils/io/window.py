507c18d316ee2a107a68976aa09bbe38a83af57e
PyDevC <anuskcse@gmail.com>
2025-04-01 20:52:26 +0530

    open_window made more sense

--- hestreg/utils/io/window.py
+++ hestreg/utils/io/window.py
@@ -1,6 +1,6 @@
 import cv2
 
-def screen_start(frame, win_name):
+def open_window(frame, win_name):
     cv2.imshow(win_name, frame)
     if cv2.waitKey(1) == ord("q"):
         cv2.destroyAllWindows()
